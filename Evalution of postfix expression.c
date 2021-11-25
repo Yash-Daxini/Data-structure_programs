0cb3ec74c0a58401164870789e331743e68886bf
Yash-Daxini7 <90504615+Yash-Daxini7@users.noreply.github.com>
2021-11-26 00:48:02 +0530

    Add files via upload

---
+++ Evalution of postfix expression.c
@@ -0,0 +1,75 @@
+#include"stdio.h"
+#include"ctype.h"
+#include"math.h"
+#define n 50
+int s[n],top=-1;
+char postfix[n];
+void push(int);
+int pop();
+int evalution(int,int,char);
+void main()
+{
+    int i,value,op1,op2;
+    char next,opr;
+    printf("Enter postfix expression:");
+    scanf("%s",postfix);
+    for(i=0;postfix[i] != '\0';i++)
+    {
+        next=postfix[i];
+        if(isdigit(next))
+        {
+            push(next-'0');
+        }
+        else if(next == '+' || next == '-' || next == '*' || next == '/' || next  == '^')
+        {
+            op2=pop();
+            op1=pop();
+            value=evalution(op1,op2,next);
+            push(value);
+        }
+    }
+    printf("Answer = %d",pop());
+}
+void push(int input)
+{
+    if(top>=n-1)
+    {
+        printf("Overflow");
+        return;
+    }
+    top=top+1;
+    s[top]=input;
+}
+int pop()
+{
+    if(top<0)
+    {
+        printf("Underflow");
+    }
+    top=top-1;
+    return(s[top+1]);
+}
+int evalution(int op1,int op2,char next)
+{
+    if(next=='+')
+    {
+        return op1+op2;
+    }
+    else if(next=='-')
+    {
+        return op1-op2;
+    }
+    else if(next=='*')
+    {
+        return op1*op2;
+    }
+    else if(next=='/')
+    {
+        return op1/op2;
+    }
+    else if(next=='^')
+    {
+        return pow(op1,op2);
+    }
+}
+
